9d3e619768e70bd39a04fdbd760dc5cd29fd0c7e
CedricMOUNIER <102512689+CedricMOUNIER@users.noreply.github.com>
2022-03-30 15:23:58 +0200

    ajout DAL categorie et retrait

--- src/main/java/fr/formation/enchere/eni/dal/CategorieDAO.java
+++ src/main/java/fr/formation/enchere/eni/dal/CategorieDAO.java
@@ -11,6 +11,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.formation.enchere.eni.bo.Categorie;
 import fr.formation.enchere.eni.bo.Retrait;
 import fr.formation.enchere.eni.dal.util.ConnectionProvider;
 
@@ -20,30 +21,29 @@ import fr.formation.enchere.eni.dal.util.ConnectionProvider;
  * @author cmounier2022
  * @date 29 mars 2022
  * @version EnchereENI- V0.1
- * @since 29 mars 2022 - 15:18:15
+ * @since 29 mars 2022 - 15:22:22
  *
  */
-public class RetraitDAO implements IRetraitDAO {
+public class CategorieDAO implements ICategorieDAO {
 
-	private final String SELECT = "SELECT no_article, rue, code_postal, ville FROM RETRAITS";
-	private final String INSERT = "INSERT INTO RETRAITS (rue, code_postal, ville) VALUES (?, ?, ?)";
-	private final String UPDATE = "UPDATE RETRAITS SET rue = ?, code_postal = ?, ville = ? WHERE no_article = ?";
-	private final String DELETE = "DELETE INTO RETRAITS WHERE no_article = ?";
-	private final String SELECT_BY_ID = "SELECT no_article, rue, code_postal, ville FROM RETRAITS WHERE no_article = ?";
+	private final String SELECT = "SELECT no_categorie, libelle FROM CATEGORIES";
+	private final String INSERT = "INSERT INTO CATEGORIES (libelle) VALUES (?)";
+	private final String UPDATE = "UPDATE CATEGORIES SET libelle = ?, WHERE no_categorie = ?";
+	private final String DELETE = "DELETE INTO CATEGORIES WHERE no_categorie = ?";
+	private final String SELECT_BY_ID = "SELECT no_categorie, libelle FROM CATEGORIES WHERE no_categorie = ?";
 
 	/**
 	 * {@inheritedDoc}
 	 */
 	@Override
-	public List<Retrait> selectAll() throws DALException {
-		List<Retrait> result = new ArrayList<Retrait>();
+	public List<Categorie> selectAll() throws DALException {
+		List<Categorie> result = new ArrayList<Categorie>();
 		try (Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(SELECT);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
-				Retrait retrait = new Retrait(rs.getInt("no_article"), rs.getString("rue"), rs.getString("code_postal"),
-						rs.getString("ville"));
-				result.add(retrait);
+				Categorie categorie = new Categorie(rs.getInt("no_categorie"), rs.getString("libelle")), rs.getString("categorie_article"));
+				result.add(categorie);
 			}
 		} catch (SQLException e) {
 			throw new DALException("Probleme de select : " + e.getMessage());
@@ -56,20 +56,19 @@ public class RetraitDAO implements IRetraitDAO {
 	 * {@inheritedDoc}
 	 */
 	@Override
-	public void insert(Retrait retrait) throws DALException {
+	public void insert(Categorie categorie) throws DALException {
 		try (Connection cnx = ConnectionProvider.getConnection()) {
 
 			PreparedStatement stmt = cnx.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
 
-			stmt.setString(1, retrait.getRue());
-			stmt.setString(2, retrait.getCodePostal());
-			stmt.setString(3, retrait.getVille());
+			stmt.setString(1, categorie.getLibelle());
+			stmt.setString(2, categorie.getCategorie_article());
 
 			Integer nb = stmt.executeUpdate();
 			if (nb > 0) {
 				ResultSet rs = stmt.getGeneratedKeys();
 				if (rs.next()) {
-					retrait.setNoArticle((rs.getInt(1)));
+					categorie.setNoCategorie((rs.getInt(1)));
 				}
 			}
 
@@ -77,7 +76,7 @@ public class RetraitDAO implements IRetraitDAO {
 			if (nb > 0) {
 				ResultSet rs = stmt.getGeneratedKeys();
 				if (rs.next()) {
-					retrait.setNoArticle((rs.getInt(1)));
+					categorie.setNoCategorie((rs.getInt(1)));
 				}
 			}
 
@@ -95,36 +94,35 @@ public class RetraitDAO implements IRetraitDAO {
 
 			PreparedStatement stmt = cnx.prepareStatement(DELETE, Statement.RETURN_GENERATED_KEYS);
 
-			stmt.setInt(1, id);
+			stmt.setInt(2, id);
 
 			stmt.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new DALException("Erreur dans la fonction delete : " + e.getMessage());
 		}
+
 	}
 
 	/**
 	 * {@inheritedDoc}
 	 */
 	@Override
-	public void update(Retrait retrait, Integer id) throws DALException {
+	public void update(Categorie categorie, Integer id) throws DALException {
 		try (Connection cnx = ConnectionProvider.getConnection()) {
 
 			PreparedStatement stmt = cnx.prepareStatement(UPDATE);
 
-			stmt.setString(1, retrait.getRue());
-			stmt.setString(2, retrait.getCodePostal());
-			stmt.setString(3, retrait.getVille());
+			stmt.setString(1, categorie.getLibelle());
+			stmt.setString(2, categorie.getCategorie_article());
 
-			stmt.setInt(4, id);
+			stmt.setInt(3, id);
 
 			stmt.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new DALException("DAL - Erreur dans la fonction update : " + e.getMessage());
 		}
-
 	}
 
 	/**
@@ -137,8 +135,7 @@ public class RetraitDAO implements IRetraitDAO {
 			PreparedStatement stmt = con.prepareStatement(SELECT_BY_ID);
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
-				result = new Retrait(rs.getInt("no_article"), rs.getString("rue"), rs.getString("code_postal"),
-						rs.getString("ville"));
+				result = new Categorie(rs.getInt("no_categorie"), rs.getString("libelle"), rs.getString("categorie_article"));
 
 			}
 		} catch (SQLException e) {
@@ -146,7 +143,6 @@ public class RetraitDAO implements IRetraitDAO {
 
 		}
 		return result;
-
 	}
 
 }
--- src/main/java/fr/formation/enchere/eni/dal/ICategorieDAO.java
+++ src/main/java/fr/formation/enchere/eni/dal/ICategorieDAO.java
@@ -5,27 +5,28 @@ package fr.formation.enchere.eni.dal;
 
 import java.util.List;
 
+import fr.formation.enchere.eni.bo.Categorie;
 import fr.formation.enchere.eni.bo.Retrait;
 
 /**
  * Classe en charge de
+ * 
  * @author msorin2022
  * @date 29 mars 2022
  * @version EnchereENI- V0.1
- * @since  29 mars 2022 - 11:30:41
+ * @since 29 mars 2022 - 11:30:28
  *
  */
-public interface IRetraitDAO {
+public interface ICategorieDAO {
 
-	public void insert(Retrait retrait) throws DALException;
-	
-	public void update(Retrait retrait, Integer id) throws DALException;
-	
-	public List<Retrait>selectAll() throws DALException;
-	
+	public void insert(Categorie categorie) throws DALException;
+
+	public void update(Categorie categorie, Integer id) throws DALException;
+
+	public List<Categorie> selectAll() throws DALException;
+
 	public void delete(Integer id) throws DALException;
-	
+
 	public void selectById(Integer id) throws DALException;
-	
-	
+
 }
