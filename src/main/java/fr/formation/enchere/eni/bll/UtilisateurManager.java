0c3974099400b2e58b0650714fae748c55bec411
CedricMOUNIER <102512689+CedricMOUNIER@users.noreply.github.com>
2022-03-30 09:31:10 +0200

    maj bll select

--- src/main/java/fr/formation/enchere/eni/bll/UtilisateurManager.java
+++ src/main/java/fr/formation/enchere/eni/bll/UtilisateurManager.java
@@ -3,6 +3,8 @@
  */
 package fr.formation.enchere.eni.bll;
 
+import java.util.List;
+
 import fr.formation.enchere.eni.bo.Utilisateur;
 import fr.formation.enchere.eni.dal.DALException;
 import fr.formation.enchere.eni.dal.DAOFact;
@@ -10,20 +12,22 @@ import fr.formation.enchere.eni.dal.IUtilisateurDAO;
 
 /**
  * Classe en charge de
+ * 
  * @author msorin2022
  * @date 29 mars 2022
  * @version EnchereENI- V0.1
- * @since  29 mars 2022 - 14:11:25
+ * @since 29 mars 2022 - 14:11:25
  *
  */
-public class UtilisateurManager implements IUtilisateurManager{
-	
+public class UtilisateurManager implements IUtilisateurManager {
+
 	private IUtilisateurDAO dao = DAOFact.getUtilisateurDAO();
 
 	/**
-	*{@inheriteDoc}
-	 * @throws BLLException 
-	*/
+	 * {@inheriteDoc}
+	 * 
+	 * @throws BLLException
+	 */
 	@Override
 	public void insert(Utilisateur utilisateur) throws BLLException {
 
@@ -33,25 +37,26 @@ public class UtilisateurManager implements IUtilisateurManager{
 			throw new BLLException("BLL - erreur dans la fonction insert : " + e.getMessage());
 		}
 	}
-		
+
 	/**
-	*{@inheritedDoc}
-	*/
+	 * {@inheritedDoc}
+	 */
 	@Override
 	public void delete(Utilisateur utilisateur) throws BLLException {
-		
+
 		try {
 			dao.delete(utilisateur);
 		} catch (DALException e) {
 			throw new BLLException("BLL - erreur dans la fonction delete : " + e.getMessage());
 		}
-		
+
 	}
 
 	/**
-	*{@inheriteDoc}
-	 * @throws BLLException 
-	*/
+	 * {@inheriteDoc}
+	 * 
+	 * @throws BLLException
+	 */
 	@Override
 	public void update(Utilisateur utilisateur) throws BLLException {
 		try {
@@ -61,4 +66,17 @@ public class UtilisateurManager implements IUtilisateurManager{
 		}
 	}
 
+	/**
+	 * {@inheritedDoc}
+	 * 
+	 * @throws BLLException
+	 */
+	@Override
+	public List<Utilisateur> selelctAll() throws BLLException {
+		try {
+			return dao.selectAll();
+		} catch (DALException e) {
+			throw new BLLException("BLL - ERREUR DANS LA FONCTION SELECT : " + e.getMessage());
+		}
+	}
 }
