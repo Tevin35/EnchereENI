a087f20eda352e0edcbd11f3650c35538c8b0303
sonic-san <tevin.abhay@gmail.com>
2022-04-06 12:05:49 +0200

    retraitmodel et ajoutretraitservlet créé

---
+++ src/main/java/fr/formation/enchere/eni/ihm/AjoutRetraitServlet.java
@@ -0,0 +1,50 @@
+package fr.formation.enchere.eni.ihm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.formation.enchere.eni.bll.ArticleManagerSing;
+import fr.formation.enchere.eni.bll.IArticleManager;
+import fr.formation.enchere.eni.bll.IRetraitManager;
+import fr.formation.enchere.eni.bll.RetraitManagerSing;
+
+/**
+ * Servlet implementation class AjoutRetraitServlet
+ */
+@WebServlet("/AjoutRetraitServlet")
+public class AjoutRetraitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private IArticleManager managerArticle = ArticleManagerSing.getInstance();
+	private IRetraitManager managerRetrait = RetraitManagerSing.getInstance();
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjoutRetraitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArticleModel modelA = new ArticleModel();
+
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
